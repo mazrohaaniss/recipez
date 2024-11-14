7218d2ef5bb9ed765a01763acca1d26d914a4118
mazrohaanis <mazrohaaniss@gmail.com>
2024-11-14 14:29:58 +0700

    Aku coba push

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+// pages/Home.js
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold text-blue-600">Welcome to RecipeZ!</h1>
+    </div>
+  );
+}
+
+export default Home;
